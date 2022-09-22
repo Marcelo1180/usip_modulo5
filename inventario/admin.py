93908763588a83c943e7b456d063a58b20e7defb
Marcelo Arteaga <arteagamarcelo@gmail.com>
2022-09-21 22:05:02 -0400

    customizando el administrador

--- inventario/admin.py
+++ inventario/admin.py
@@ -5,7 +5,13 @@ from .models import Orden
 from .models import OrdenProducto
 
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "precio", "unidades")
+    ordering = ["precio"]
+    search_fields = ["nombre"]
+    list_filter = ("disponible","precio")
+
 admin.site.register(Categoria)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Orden)
 admin.site.register(OrdenProducto)
